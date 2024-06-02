ce79ed3c76fb1ed02f03ec71b717a4d465f1278d
Charlscayabyab27 <202080463@psu.palawan.edu.ph>
2024-06-03 00:43:11 +0800

    finals

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# WEBSYSTEM COFFEE PHP 
+
+```
+CHARLS BRENT CAYABYAB
+
+4TH Year Student at Palawan State Univerity
+
+EMAIL:202080463@psu.palawan.edu.ph
---
+++ index.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Lili's Cafe</title>
+    <!--CSS-->
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet"
+    href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>  
+<body>
+    <!--navbar-->
+    <header>
+            <a href="#" class="logo">
+            <img src="img/logo.png" alt="">
+        </a>
+    <!--menu icon-->
+            <i class='bx bx-menu' id="menu-icon"></i>
+    <!--link-->
+            <ul class="navbar">
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#menu">Menu</a></li>
+                <li><a href="#apps">Apps</a></li>
+                <li><a href="#contact">Contact</a></li>
+                <li><a href="#Customers">Customers</a></li>
+        </ul>
+        </ul>
+        <!--Icon-->
+        <div class="header-icon">
+            <i class='bx bx-cart' ></i>
+            <i class='bx bx-search-alt' id="search-icon" ></i>
+        </div>
+        <!--search bar-->
+        <div class="search-box">
+            <input type="search" name="" id="" placeholder="Search Here">
+        </div>
+        
+    </header>
+    <!--home-->
+    <section class="home" id="home">
+        <div class="home-text">
+            <h1>Start your day with a black coffee</h1>
+        <p>Choose and taste delicious coffee from the best beans.</p>
+        <a href="#" class="btn">Shop Now</a>
+        </div>
+        <div class="home-img">
+            <img src="img/blackcoffee2.jpg" alt="">
+        </div>
+    </section>
+    <!--about-->
+    <section class="about" id="about">
+        <div class="about-img">
+            <img src="img/closeup.jpg" alt="">
+        </div>
+        <div class="about-text">
+            <h2>Our History</h2>
+            <p>Lili's Cafe was established on June 30, 2024, with Charls Brent Cayabyab as its owner. His motivation for founding this cafe stemmed from his admiration for his Love </p>
+            <p>"Like the perfect cup of coffee, my feelings for you are rich, warm, and impossible to resist."</p>
+            <a href="#" class="btn">Learn More</a>
+        </div>
+
+    </section>
+    <!--Products-->
+    <section class="menu" id="menu">
+        <div class="heading">
+            <h2>Our Popular Menu</h2>
+        </div>
+        <!--container-->
+        <div class="product-container">
+            <div class="box">
+                <img src="img/americano.jpg" alt="">
+                    <h3>Americano coffee</h3>
+                    <div class="content">
+                        <span>$25</span>
+                        <a href="#"> Add to Cart</a>
+                    </div>
+            </div>
+            <div class="product-container">
+                <div class="box">
+                    <img src="img/americano.jpg" alt="">
+                        <h3>Americano coffee</h3>
+                        <div class="content">
+                            <span>$25</span>
+                            <a href="#"> Add to Cart</a>
+                        </div>
+                </div>
+                <div class="product-container">
+                    <div class="box">
+                        <img src="img/americano.jpg" alt="">
+                            <h3>Americano coffee</h3>
+                            <div class="content">
+                                <span>$25</span>
+                                <a href="#"> Add to Cart</a>
+                            </div>
+                    </div>
+        </div>
+    </section>
+    <!---customer-->
+<section class="customers" id="Customers">
+        <div class="heading">
+            <h2> Our Customer's </h2>
+        </div>
+
+<!---Container Customers-->
+        <div class="row">
+            <div class=" col">
+                <div class="customers-container">
+                    <div class="box"> 
+                        <div class="stars">
+                            <i class= 'bx bxs-star' ></i>
+                            <i class= 'bx bxs-star' ></i>
+                            <i class= 'bx bxs-star' ></i>
+                            <i class= 'bx bxs-star' ></i>
+                            <i class= 'bx bxs-star' ></i>   
+                        </div>
+                        <p>
+                            the coffee is made with love 
+                        </p>
+                        <h2>Lalisa Manoban</h2>
+                        <img src="img/lisa1.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class=" col">
+                <div class="customers-container">
+                    <div class="box"> 
+                        <div class="stars">
+                            <i class= 'bx bxs-star' ></i>
+                            <i class= 'bx bxs-star' ></i>
+                            <i class= 'bx bxs-star' ></i>
+                            <i class= 'bx bxs-star' ></i>
+                            <i class= 'bx bxs-star' ></i>   
+                        </div>
+                        <p>
+                            the coffee is made with love 
+                        </p>
+                        <h2>Czarlette Rancap</h2>
+                        <img src="">
+                    </div>
+                </div>
+            </div>
+            <div class=" col">
+                <div class="customers-container">
+                    <div class="box"> 
+                        <div class="stars">
+                            <i class= 'bx bxs-star' ></i>
+                            <i class= 'bx bxs-star' ></i>
+                            <i class= 'bx bxs-star' ></i>
+                            <i class= 'bx bxs-star' ></i>
+                            <i class= 'bx bxs-star' ></i>   
+                        </div>
+                        <p>
+                            the coffee is made with love 
+                        </p>
+                        <h2>Charls Cayabyab</h2>
+                        <img src="">
+                    </div>
+                </div>
+            </div>
+        
+        
+        </div>
+    </section>
+    <section class="footer">
+        <div class="footer-box">
+                <h3> Lili's Cafe</h3>
+                <p>Lilis's Cafe is popular destination for individuals looking to work, study, or socialize in a comfortable setting. Many offer free Wi-Fi access, making them ideal spots for remote work or casual meetings. Some coffee shops also host events such as live music performances, poetry readings, or art exhibitions to further enhance the community atmosphere.</p>
+                <div class="social">
+                    <a href="#"><i class='bx bxl-facebook'></i></a>
+                    <a href="#"><i class='bx bxl-twitter'></i></a>
+                    <a href="#"><i class='bx bxl-instagram'></i></a>
+                    <a href="#"><i class='bx bxl-tiktok'></i></a>
+
+                </div>
+            </div>
+            <div class="footer-box">
+                <h2>Support</h2>
+                <li><a href="#">Product</a></li>
+                <li><a href="#">Help & Support</a></li>
+                <li><a href="#">Return Policy</a></li>
+                <li><a href="#">Terms of Use</a></li>
+                <li><a href="#">Product</a></li>
+            </div>
+            <div class="footer-box">
+                <h3>View Guides</h3>
+                <li><a href="#">Features</a></li>
+                <li><a href="#">Careers</a></li>
+                <li><a href="#">Blog Post</a></li>
+                <li><a href="#">Our Branches</a></li>
+                
+            </div>
+            <div class="footer-box">
+                <h3>Contact</h3>
+                <div class="contact">
+                    <span><i class='bx bxs-map'></i>1 Pl. de la Bourse, 44000 Nantes, France</span>
+                    <span><i class='bx bxs-phone'></i>+639158444426</span>
+                    <span><i class='bx bxs-envelope'></i>202080463@psu.palawan.edu.ph</span>
+                </div>
+            </div>
+    </section>    
+
+    <!--JS link-->
+    <script src="main.js"></script>
+</body>
+</html>
+
